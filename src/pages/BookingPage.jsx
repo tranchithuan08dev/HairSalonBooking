fe4b94328e285e03dc8f97ca76924a68757711f8
ChiThuan <tranchithuanjr@gmail.com>
2024-09-24 22:31:10 +0700

    check booking

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/header";
+import Footer from "../components/Footer";
 function BookingPage() {
   return (
     <>
@@ -310,46 +311,7 @@ function BookingPage() {
               pointment
             </p>
           </form>
-          <footer className="bg-dark text-center text-lg-start text-white">
-            {/* Grid container */}
-            <div className="container p-4" style={{ marginTop: "-20px" }}>
-              {/*Grid row*/}
-              <div className="row">
-                {/*Grid column*/}
-                <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-                  <h5 className="text-uppercase">Footer text</h5>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Iste atque ea quis molestias. Fugiat pariatur maxime quis
-                    culpa corporis vitae repudiandae aliquam voluptatem veniam,
-                    est atque cumque eum delectus sint!
-                  </p>
-                </div>
-                {/*Grid column*/}
-                {/*Grid column*/}
-                <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-                  <h5 className="text-uppercase">Footer text</h5>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Iste atque ea quis molestias. Fugiat pariatur maxime quis
-                    culpa corporis vitae repudiandae aliquam voluptatem veniam,
-                    est atque cumque eum delectus sint!
-                  </p>
-                </div>
-                {/*Grid column*/}
-              </div>
-              {/*Grid row*/}
-            </div>
-            {/* Grid container */}
-            {/* Copyright */}
-            <div
-              className="text-center p-3"
-              style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-            >
-              © 2020 Copyright: &gt;
-            </div>
-            {/* Copyright */}
-          </footer>
+          <Footer />
         </div>
       </div>
     </>
