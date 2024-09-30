0b694093a8a4b935a78143913b1f367298ad9d7d
ChiThuan <tranchithuanjr@gmail.com>
2024-10-01 01:29:51 +0700

    create ins

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -584,6 +584,76 @@ function HomePage() {
         </div>
 
         {/* Blog End */}
+        {/* Ins */}
+        <div className="container mx-auto ">
+          <div className="text-center">
+            <h2 className="service-title text-black">Instagram</h2>
+          </div>
+          <div className="gallery">
+            {/* Images */}
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 1"
+              className="custom-img "
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 2"
+              className="custom-img"
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 3"
+              className="custom-img"
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 4"
+              className="custom-img"
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 5"
+              className="custom-img"
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 6"
+              className="custom-img"
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 7"
+              className="custom-img"
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 8"
+              className="custom-img"
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 9"
+              className="custom-img"
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 10"
+              className="custom-img"
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 11"
+              className="custom-img"
+            />
+            <img
+              src="../assets/image/avatar.jpg"
+              alt="Image 12"
+              className="custom-img"
+            />
+          </div>
+        </div>
+        {/* Ins end */}
         {/* Map  */}
         <div className="container mx-auto py-8">
           <div className="text-center mb-6">
