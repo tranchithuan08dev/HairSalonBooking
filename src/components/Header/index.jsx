4f8d7e1160bcaad16dae666dd23d6da82551b7d9
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-01 16:38:45 +0700

    update header footer

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,44 +1,44 @@
-// import React from "react";
-// import "./header.css";
-// function Header() {
-//   return (
-//     <>
-//       <nav className="navbar navbar-expand-lg navbar-light backgroud-header">
-//         <div className="container mx-auto">
-//           {/* Left Side */}
-//           <ul className="navbar-nav me-auto">
-//             <li className="nav-item">
-//               <a className="nav-link text-white" href="#">
-//                 Contact
-//               </a>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link text-white" href="#">
-//                 Feedback
-//               </a>
-//             </li>
-//           </ul>
-//           {/* Center*/}
-//           <a className="navbar-brand mx-auto" href="#">
-//             <h1 className="text-white">Harmony</h1>
-//           </a>
-//           {/* Right Side */}
-//           <ul className="navbar-nav ms-auto">
-//             <li className="nav-item">
-//               <a className="nav-link text-white" href="#">
-//                 Booking
-//               </a>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link text-white" href="#">
-//                 Service
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//       </nav>
-//     </>
-//   );
-// }
+import React from "react";
+import "./header.css";
+function Header() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light backgroud-header">
+        <div className="container mx-auto">
+          {/* Left Side */}
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                Contact
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                Feedback
+              </a>
+            </li>
+          </ul>
+          {/* Center*/}
+          <a className="navbar-brand mx-auto" href="#">
+            <h1 className="text-white">Harmony</h1>
+          </a>
+          {/* Right Side */}
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                Booking
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">
+                Service
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
 
-// export default Header;
+export default Header;
