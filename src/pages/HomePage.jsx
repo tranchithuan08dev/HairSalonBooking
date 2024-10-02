2b7ebd2dc31647eb39e974f06fddd14769d59a70
ChiThuan <tranchithuanjr@gmail.com>
2024-10-02 12:14:57 +0700

    create component stylist home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import Booking from "../components/Home/Booking";
 import Service from "../components/Home/Service";
 import SroteSpace from "../components/Home/StoreSpace";
+import Stylist from "../components/Home/Stylist";
 
 function HomePage() {
   return (
@@ -24,147 +25,7 @@ function HomePage() {
         <SroteSpace />
         {/* End Space */}
         {/* Stylist */}
-        <div className="service" style={{ backgroundColor: "#2e2d2d" }}>
-          <div className="text-center">
-            <h2 className="service-title text-white">Stylist</h2>
-          </div>
-          <div className="container mx-auto">
-            {/* Row 1 */}
-            <div className="row d-flex justify-content-center flex-wrap">
-              <div className="col-md-4 d-flex justify-content-center mb-4">
-                <div className="card">
-                  <img
-                    src="../assets/image/logo_booking.jpg"
-                    className="card-img-top"
-                    alt="Stylist Trần Chí Thuận"
-                    style={{ height: 240, objectFit: "cover" }}
-                  />
-                  <div className="card-body">
-                    <h3 className="text-center">
-                      <a
-                        className="text-black"
-                        href="./StylistDetail.html"
-                        style={{ textDecoration: "none" }}
-                      >
-                        Trần Chí Thuận
-                      </a>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 d-flex justify-content-center mb-4">
-                <div className="card">
-                  <img
-                    src="../assets/image/logo_booking.jpg"
-                    className="card-img-top"
-                    alt="Stylist Trần Chí Thuận"
-                    style={{ height: 240, objectFit: "cover" }}
-                  />
-                  <div className="card-body">
-                    <h3 className="text-center">
-                      <a
-                        className="text-black"
-                        href="./StylistDetail.html"
-                        style={{ textDecoration: "none" }}
-                      >
-                        Trần Chí Thuận
-                      </a>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 d-flex justify-content-center mb-4">
-                <div className="card">
-                  <img
-                    src="../assets/image/logo_booking.jpg"
-                    className="card-img-top"
-                    alt="Stylist Trần Chí Thuận"
-                    style={{ height: 240, objectFit: "cover" }}
-                  />
-                  <div className="card-body">
-                    <h3 className="text-center">
-                      <a
-                        className="text-black"
-                        href="./StylistDetail.html"
-                        style={{ textDecoration: "none" }}
-                      >
-                        Trần Chí Thuận
-                      </a>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 d-flex justify-content-center mb-4">
-                <div className="card">
-                  <img
-                    src="../assets/image/logo_booking.jpg"
-                    className="card-img-top"
-                    alt="Stylist Trần Chí Thuận"
-                    style={{ height: 240, objectFit: "cover" }}
-                  />
-                  <div className="card-body">
-                    <h3 className="text-center">
-                      <a
-                        className="text-black"
-                        href="./StylistDetail.html"
-                        style={{ textDecoration: "none" }}
-                      >
-                        Trần Chí Thuận
-                      </a>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 d-flex justify-content-center mb-4">
-                <div className="card">
-                  <img
-                    src="../assets/image/logo_booking.jpg"
-                    className="card-img-top"
-                    alt="Stylist Trần Chí Thuận"
-                    style={{ height: 240, objectFit: "cover" }}
-                  />
-                  <div className="card-body">
-                    <h3 className="text-center">
-                      <a
-                        className="text-black"
-                        href="./StylistDetail.html"
-                        style={{ textDecoration: "none" }}
-                      >
-                        Trần Chí Thuận
-                      </a>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 d-flex justify-content-center mb-4">
-                <div className="card">
-                  <img
-                    src="../assets/image/logo_booking.jpg"
-                    className="card-img-top"
-                    alt="Stylist Trần Chí Thuận"
-                    style={{ height: 240, objectFit: "cover" }}
-                  />
-                  <div className="card-body">
-                    <h3 className="text-center">
-                      <a
-                        className="text-black"
-                        href="./StylistDetail.html"
-                        style={{ textDecoration: "none" }}
-                      >
-                        Trần Chí Thuận
-                      </a>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="d-flex justify-content-center p-5">
-            <a href="#" className="loading">
-              Xem thêm
-            </a>
-          </div>
-        </div>
+        <Stylist />
 
         {/* Stylist End */}
         {/* Take care */}
