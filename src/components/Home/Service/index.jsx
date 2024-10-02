57f8837a68fd5de8300e1501c5362399bf0a0ec8
ChiThuan <tranchithuanjr@gmail.com>
2024-10-02 14:07:34 +0700

    create compenent cardItem and update new blog,stylist,service

--- src/components/Home/Service/index.jsx
+++ src/components/Home/Service/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Item from "../CardItem";
 
 function Service() {
   return (
@@ -10,156 +11,12 @@ function Service() {
         <div className="container mx-auto">
           {/* Row 1 */}
           <div className="row d-flex justify-content-center flex-wrap">
-            <div className="col-md-4 d-flex justify-content-center mb-4">
-              <div className="card">
-                <img
-                  src="../assets/image/logo_booking.jpg"
-                  className="card-img-top"
-                  alt="Stylist Trần Chí Thuận"
-                  style={{ height: 240, objectFit: "cover" }}
-                />
-                <div className="card-body">
-                  <h3 className="text-center">
-                    <a
-                      className="text-black"
-                      href="./StylistDetail.html"
-                      style={{ textDecoration: "none" }}
-                    >
-                      Trần Chí Thuận
-                    </a>
-                  </h3>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4 d-flex justify-content-center mb-4">
-              <div className="card">
-                <img
-                  src="../assets/image/logo_booking.jpg"
-                  className="card-img-top"
-                  alt="Stylist Trần Chí Thuận"
-                  style={{ height: 240, objectFit: "cover" }}
-                />
-                <div className="card-body">
-                  <h3 className="text-center">
-                    <a
-                      className="text-black"
-                      href="./StylistDetail.html"
-                      style={{ textDecoration: "none" }}
-                    >
-                      Trần Chí Thuận
-                    </a>
-                  </h3>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4 d-flex justify-content-center mb-4">
-              <div className="card">
-                <img
-                  src="../assets/image/logo_booking.jpg"
-                  className="card-img-top"
-                  alt="Stylist Trần Chí Thuận"
-                  style={{ height: 240, objectFit: "cover" }}
-                />
-                <div className="card-body">
-                  <h3 className="text-center">
-                    <a
-                      className="text-black"
-                      href="./StylistDetail.html"
-                      style={{ textDecoration: "none" }}
-                    >
-                      Trần Chí Thuận
-                    </a>
-                  </h3>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4 d-flex justify-content-center mb-4">
-              <div className="card">
-                <img
-                  src="../assets/image/logo_booking.jpg"
-                  className="card-img-top"
-                  alt="Stylist Trần Chí Thuận"
-                  style={{ height: 240, objectFit: "cover" }}
-                />
-                <div className="card-body">
-                  <h3 className="text-center">
-                    <a
-                      className="text-black"
-                      href="./StylistDetail.html"
-                      style={{ textDecoration: "none" }}
-                    >
-                      Trần Chí Thuận
-                    </a>
-                  </h3>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4 d-flex justify-content-center mb-4">
-              <div className="card">
-                <img
-                  src="../assets/image/logo_booking.jpg"
-                  className="card-img-top"
-                  alt="Stylist Trần Chí Thuận"
-                  style={{ height: 240, objectFit: "cover" }}
-                />
-                <div className="card-body">
-                  <h3 className="text-center">
-                    <a
-                      className="text-black"
-                      href="./StylistDetail.html"
-                      style={{ textDecoration: "none" }}
-                    >
-                      Trần Chí Thuận
-                    </a>
-                  </h3>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4 d-flex justify-content-center mb-4">
-              <div className="card">
-                <img
-                  src="../assets/image/logo_booking.jpg"
-                  className="card-img-top"
-                  alt="Stylist Trần Chí Thuận"
-                  style={{ height: 240, objectFit: "cover" }}
-                />
-                <div className="card-body">
-                  <h3 className="text-center">
-                    <a
-                      className="text-black"
-                      href="./StylistDetail.html"
-                      style={{ textDecoration: "none" }}
-                    >
-                      Trần Chí Thuận
-                    </a>
-                  </h3>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
           </div>
         </div>
         <div className="d-flex justify-content-center p-5">
