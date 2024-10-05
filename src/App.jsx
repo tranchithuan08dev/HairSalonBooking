4a852b2908cfc35dd2513574da70c80bdcf734d4
ChiThuan <tranchithuanjr@gmail.com>
2024-10-05 16:04:34 +0700

    create route dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Home from "./pages/Dashboard/Home";
+import DashBroad from "./pages/Dashboard";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+
+        <Route element={<DashBroad />}>
+          <Route
+            path="/dashboard"
+            element={<Navigate to="/dashboard/home" />}
+          />
+          <Route path="/dashboard/home" element={<Home />} />
+        </Route>
       </Routes>
     </>
   );
