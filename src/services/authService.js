4a4617c1eb5e6834ef301c52595ec7dcdbf3f25f
ChiThuan <tranchithuanjr@gmail.com>
2024-10-08 00:21:26 +0700

    create fetchmewith

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,5 +4,8 @@ const authService = {
   login: (data) => {
     return API.call().post(`/api/v1/auth/login`, data);
   },
+  fetchWithMe: (token) => {
+    return API.callWithToken(token).get(`/api/v1/user/getCurrent`);
+  },
 };
 export default authService;
