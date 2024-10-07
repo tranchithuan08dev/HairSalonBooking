3f5344468ea23ddfccf5fa8d316f4bbaadeb8d4b
ChiThuan <tranchithuanjr@gmail.com>
2024-10-08 00:15:27 +0700

    create callwithtoken

--- src/services/api.js
+++ src/services/api.js
@@ -6,5 +6,13 @@ const API = {
       baseURL: "http://localhost:5000/",
     });
   },
+  callWithToken: function (token) {
+    return axios.create({
+      baseURL: "http://localhost:5000/",
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
 };
 export default API;
