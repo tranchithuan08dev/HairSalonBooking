4e4bd92540540b72ea92dd6961b17c553ffa34b7
ChiThuan <tranchithuanjr@gmail.com>
2024-10-08 01:08:00 +0700

    update loginSlice

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -11,18 +11,15 @@ const name = "auth";
 export const Login = createAsyncThunk(`${name}/Login`, async (params = {}) => {
   try {
     const res = await authService.login(params);
-    console.log("res", res);
-    // Simulate an API call or fetch request here
-    // Example: return await fetch('/login', { method: 'POST', body: JSON.stringify(params) });
+    const token = res.data.records.accessToken;
+    const currentUser = await authService.fetchWithMe(token);
+    const currenInfor = currentUser.data;
 
     return {
       ok: true,
       data: {
-        token: "example-token",
-        currenInfor: {
-          name: "John Doe",
-          email: "john@example.com",
-        },
+        token,
+        currenInfor,
       },
     };
   } catch (error) {
@@ -40,12 +37,8 @@ const authSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(Login.fulfilled, (state, action) => {
       if (action.payload.ok) {
-        console.log("action", action.payload.ok);
-
         state.token = action.payload.data.token;
-        localStorage.setItem("ACCESS_TOKEN", state.token); // Correct typo from ACCESS_TOKKEN
         state.currentUser = action.payload.data.currenInfor;
-        console.log(" state.currentUser", state.currentUser);
       }
     });
   },
