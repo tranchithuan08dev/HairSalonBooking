16b35922354c8fcc4e4fc72261ef9caca43d4e82
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-08 21:12:27 +0700

    update homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Staff/Home";
 import BookingDetail from "./pages/Staff/BookingDetail";
 import Profile from "./pages/Staff/Profile";
 import Salary from "./pages/Staff/Salary";
-// import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 import CreateBooking from "./pages/Staff/CreateBooking";
 // import BookingPage from "./pages/BookingPage";
 
