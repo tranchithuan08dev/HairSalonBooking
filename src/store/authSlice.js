ae6f172dc8acf24615ce7512c50b57d3552f0642
ChiThuan <tranchithuanjr@gmail.com>
2024-10-09 00:54:40 +0700

    save token localstorage

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -25,7 +25,27 @@ export const Login = createAsyncThunk(`${name}/Login`, async (params = {}) => {
   } catch (error) {
     return {
       ok: false,
-      message: "Thông tin đăng nhập của bạn không đúng!",
+      message: "Your login information is incorrect!",
+    };
+  }
+});
+
+export const fetchMe = createAsyncThunk(`${name}/fetchMe`, async (token) => {
+  try {
+    if (!token) token = localStorage.getItem("ACCESS_TOKKEN");
+    const currentUser = await authService.fetchWithMe(token);
+    const currenInfor = currentUser.data;
+    return {
+      ok: true,
+      data: {
+        token,
+        currenInfor,
+      },
+    };
+  } catch (error) {
+    localStorage.removeItem("ACCESS_TOKKEN");
+    return {
+      ok: false,
     };
   }
 });
@@ -39,6 +59,12 @@ const authSlice = createSlice({
       if (action.payload.ok) {
         state.token = action.payload.data.token;
         localStorage.setItem("ACCESS_TOKKEN", state.token);
+        state.currentUser = action.payload.data.currenInfor;
+      }
+    });
+    builder.addCase(fetchMe.fulfilled, (state, action) => {
+      if (action.payload.ok) {
+        state.token = action.payload.data.token;
         state.currentUser = action.payload.data.currenInfor;
       }
     });
