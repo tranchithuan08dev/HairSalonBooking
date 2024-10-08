be1329cd578029cc524a6bdb2dc38dc25cd42206
ChiThuan <tranchithuanjr@gmail.com>
2024-10-09 00:50:28 +0700

    add useEffect fetch me

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/Login/LoginPage";
+import { useDispatch } from "react-redux";
+import { fetchMe } from "./store/authSlice";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMe());
+  }, []);
   return (
     <>
       <Routes>
