66724ff6680d3985b97754cfbe3448d831950d12
ChiThuan <tranchithuanjr@gmail.com>
2024-10-11 01:40:55 +0700

    create reducer DASHBOARD

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import dashbroadSlice from "./dashbroadSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    DASHBOARD: dashbroadSlice,
+  },
 });
 
 export default store;
