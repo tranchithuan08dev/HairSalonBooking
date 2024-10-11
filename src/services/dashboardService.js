8bc0227e24892afd0b03a9031137a8f3405519e6
ChiThuan <tranchithuanjr@gmail.com>
2024-10-12 02:08:07 +0700

    create fetch getDetailStylist

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -4,6 +4,9 @@ const dashboardService = {
   getAllStylist: () => {
     return API.call().get(`/api/v1/stylist/getAll`);
   },
+  getDetailStylistById: (id) => {
+    return API.call().get(`/api/v1/stylist/detail?id=${id}`);
+  },
 };
 
 export default dashboardService;
