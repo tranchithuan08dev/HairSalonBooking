693f92043d48725a8542c909df1a11d6b097f3b2
ChiThuan <tranchithuanjr@gmail.com>
2024-10-13 20:06:23 +0700

    update page User

--- src/pages/Dashboard/User/index.jsx
+++ src/pages/Dashboard/User/index.jsx
@@ -1,11 +1,41 @@
-import React, { useEffect } from "react";
-import { Space, Table, Tag } from "antd";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Drawer,
+  Form,
+  Input,
+  Space,
+  Table,
+  Image,
+  InputNumber,
+  Tag,
+  Radio,
+  DatePicker,
+} from "antd";
+
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPostCustomer } from "../../../store/dashbroadSlice";
-import customParseFormat from "dayjs/plugin/customParseFormat";
-import dayjs from "dayjs";
+
+dayjs.extend(customParseFormat);
+const dateFormat = "YYYY/MM/DD";
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
 
 const User = () => {
+  const [open, setOpen] = useState(false);
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
+
+  const [form] = Form.useForm();
   dayjs.extend(customParseFormat);
   const dateFormat = "YYYY/MM/DD";
   const dataCustomer = useSelector((state) => state.DASHBOARD.postCustomer);
@@ -16,6 +46,21 @@ const User = () => {
   }, [dispatch]);
 
   if (dataCustomer == null) return <></>;
+  const showLargeDrawer = (stylist) => {
+    setSelectedCustomer(stylist);
+
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setSelectedCustomer(null);
+    setOpen(false);
+  };
+
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
   const columns = [
     {
       title: "Name",
@@ -51,9 +96,14 @@ const User = () => {
       title: "Action",
       key: "action",
       render: (_, record) => (
-        <Space size="middle">
-          <a>Disable</a>
-        </Space>
+        <span>
+          <Button
+            style={{ color: "blue" }}
+            onClick={() => showLargeDrawer(record.key)}
+          >
+            Detail
+          </Button>
+        </span>
       ),
     },
   ];
@@ -65,11 +115,119 @@ const User = () => {
     point: index.loyaltyPoints,
     createat: dayjs(index.createdAt).format(dateFormat),
   }));
-
   return (
     <>
-      <Table columns={columns} dataSource={data} />;
+      <Table columns={columns} dataSource={data} />
+      <Drawer
+        title="Detail Stylist"
+        placement="right"
+        width={720}
+        onClose={onClose}
+        open={open}
+        extra={
+          <Space>
+            <Button onClick={onClose}>Cancel</Button>
+          </Space>
+        }
+      >
+        <Form
+          form={form}
+          {...layout}
+          name="nest-messages"
+          onFinish={onFinish}
+          style={{
+            maxWidth: 600,
+          }}
+        >
+          <Form.Item
+            wrapperCol={{
+              offset: 4,
+              span: 20,
+            }}
+          >
+            <Space size={12}>
+              <Image
+                width={200}
+                src={avatarUrl || "https://via.placeholder.com/200"}
+                style={{
+                  borderRadius: "50%",
+                  overflow: "hidden",
+                  width: "200px",
+                  height: "200px",
+                }}
+              />
+            </Space>
+          </Form.Item>
+          <Form.Item name="fullName" label="Stylist Name">
+            <Input />
+          </Form.Item>
+          <Form.Item label="Gender" name="gender">
+            <Radio.Group>
+              <Radio value="Male">Male</Radio>
+              <Radio value="Female">Female</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item name="yob" label="Date of birth">
+            <DatePicker format={dateFormat} />
+          </Form.Item>
+          <Form.Item
+            label="Phone"
+            name="phoneNumber"
+            rules={[
+              {
+                required: true,
+                message: "Please input your phone number!",
+              },
+              {
+                pattern: /^[0-9]{10}$/,
+                message: "Phone number must be 10 digits!",
+              },
+            ]}
+          >
+            <Input type="text" placeholder="Phone" />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                message: "Please input your email!",
+              },
+              {
+                type: "email",
+                message: "The input is not a valid email!",
+              },
+            ]}
+          >
+            <Input type="email" placeholder="Email" />
+          </Form.Item>
+
+          <Form.Item name="address" label="Address">
+            <Input.TextArea />
+          </Form.Item>
+          <Form.Item name="level" label="Level">
+            <InputNumber />
+          </Form.Item>
+          <Form.Item name="status" label="Status">
+            <Radio.Group>
+              <Radio value={true}>True</Radio>
+              <Radio value={false}>False</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              ...layout.wrapperCol,
+              offset: 8,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Edit Service
+            </Button>
+          </Form.Item>
+        </Form>
+      </Drawer>
     </>
   );
 };
+
 export default User;
