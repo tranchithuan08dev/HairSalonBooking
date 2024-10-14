550a0d90c06c994baa7d62c21d1a173a322f6599
ChiThuan <tranchithuanjr@gmail.com>
2024-10-14 16:18:18 +0700

    create  sendEmail

--- src/services/authService.js
+++ src/services/authService.js
@@ -7,5 +7,8 @@ const authService = {
   fetchWithMe: (token) => {
     return API.callWithToken(token).get(`/api/v1/user/getCurrent`);
   },
+  sendEmail: (email) => {
+    return API.call().post(`/api/v1/auth/forgot-password`, email);
+  },
 };
 export default authService;
