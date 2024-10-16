fe166199cf339d979b67889a9a8959ba165883e4
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-16 22:00:59 +0700

    update layout

--- src/components/Stylist/StylistLayout/index.jsx
+++ src/components/Stylist/StylistLayout/index.jsx
@@ -1,14 +1,12 @@
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Sidebar from '../Sidebar';
 
 function Layout() {
-    const { id } = useParams(); 
-
     return (
         <>
             <div className="layout-default" style={{display: "flex"}}>
                 <div className="layout-default__sidebar" style={{width: "280px"}}>
-                    <Sidebar id={id} /> 
+                    <Sidebar/> 
                 </div>
                 <div className="layout-default__main" style={{width: "1449px", overflowY: 'hidden', overflowX: 'hidden'}}>
                     <Outlet />
