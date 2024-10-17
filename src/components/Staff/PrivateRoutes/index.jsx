39eee52833d2cabf9799cd62f7956cacefa205ac
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-17 12:20:12 +0700

    update components

--- src/components/Staff/PrivateRoutes/index.jsx
+++ src/components/Staff/PrivateRoutes/index.jsx
@@ -3,9 +3,11 @@ import { Outlet, Navigate } from "react-router-dom";
 
 function PrivateRoutes() {
     const { token, currentUser } = useSelector((state) => state.AUTH);
+    
 
-    const isLogin = !!token
-    const isStaff = currentUser?.role === "Staff";
+    const isLogin = !!token;
+    const isStaff = currentUser?.record.role === "Staff";
+    console.log(isStaff);
 
     return isLogin && isStaff ? <Outlet /> : <Navigate to="/login" />;
 }
