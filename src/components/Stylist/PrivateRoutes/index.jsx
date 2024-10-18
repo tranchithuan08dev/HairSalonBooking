9315021259894f1489a730ac3df1a388a03c49a4
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-18 12:42:18 +0700

    update private routes

--- src/components/Stylist/PrivateRoutes/index.jsx
+++ src/components/Stylist/PrivateRoutes/index.jsx
@@ -5,7 +5,7 @@ import { Outlet, Navigate } from "react-router-dom";
 function PrivateRoutes() {
     const { token, currentUser } = useSelector((state) => state.AUTH);
     const isLogin = !!token
-    const isStylist = currentUser?.role === "Stylist" ; 
+    const isStylist = currentUser?.record.role === "Stylist" ; 
     return (
         <>
             {isLogin && isStylist ? <Outlet /> : <Navigate to="/login" />}
