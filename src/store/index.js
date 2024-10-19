1947f251e9ff70752260f9b884f970efc99303ce
ChiThuan <tranchithuanjr@gmail.com>
2024-10-19 20:49:18 +0700

    create home reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dashbroadSlice from "./dashbroadSlice";
-
+import homeSlice from "./homeSlice";
 import authReducer from "./authSlice";
 
 const store = configureStore({
   reducer: {
     DASHBOARD: dashbroadSlice,
     AUTH: authReducer,
+    HOME: homeSlice,
   },
 });
 
