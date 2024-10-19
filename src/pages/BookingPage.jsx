9432dc27e0733cded77acbbb6328e0243c92b8bd
ChiThuan <tranchithuanjr@gmail.com>
2024-10-19 20:47:57 +0700

    fix import header

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Header from "../components/header";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../assets/css/booking.css";
+
 function BookingPage() {
   return (
     <>
