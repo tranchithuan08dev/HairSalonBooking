e73230642cf7297e05b3f107ca2219b57afc9bee
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-20 23:27:26 +0700

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import StylistLayout from "./components/Stylist/StylistLayout";
 import WorkShift from "./pages/Stylist/Workshift";
 import Profile from "./pages/Stylist/Profile";
 import Salary from "./pages/Stylist/Salary";
+import BookingDetail from "./pages/Stylist/BookingDetail";
 
 function App() {
   const dispatch = useDispatch();
@@ -24,17 +25,19 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
 
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+
         <Route path="/stylist/" element={<PrivateRoutes />}>
           <Route element={<StylistLayout />}>
             <Route index element={<WorkShift />} />
             <Route path="profile" element={<Profile />} />
             <Route path="salary" element={<Salary />} />
+            <Route path="bookingDetail" element={<BookingDetail />} />
           </Route>
         </Route>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/forgotPassword" element={<ForgotPassword />} />
-        <Route path="/resetpassword" element={<ResetPassword />} />
       </Routes>
     </>
   );
