d983fcffd9db9156fa01c0562243c322da27a857
ChiThuan <tranchithuanjr@gmail.com>
2024-10-20 15:42:35 +0700

    add data stylist and data Service

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../assets/css/booking.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchPostService,
+  fetchPostStylist,
+  fetchPostStylistDetailById,
+} from "../store/dashbroadSlice";
 
 function BookingPage() {
+  const dispatch = useDispatch();
+  // Call Data
+  const dataService = useSelector((state) => state.DASHBOARD.postService);
+  const dataStylist = useSelector((state) => state.DASHBOARD.postStylist);
+  const dataStylistById = useSelector(
+    (state) => state.DASHBOARD.postStylistDetailById
+  );
+
+  console.log("dataService", dataService);
+  console.log("dataStylist", dataStylist);
+  console.log("dataStylistById", dataStylistById);
+  // USE State
+  const [selectedServices, setSelectedServices] = useState([]);
+  const [selects, setSelects] = useState([1]);
+  const [selectedStylist, setSelectedStylist] = useState({});
+
+  const handleServiceChange = (index, value) => {
+    const newSelectedServices = [...selectedServices];
+    newSelectedServices[index] = value;
+    setSelectedServices(newSelectedServices);
+  };
+
+  const handleStylistChange = (event) => {
+    const stylistId = event.target.value;
+    setSelectedStylist(stylistId);
+    dispatch(fetchPostStylistDetailById(stylistId));
+  };
+  console.log("id Stylist", selectedStylist);
+
+  useEffect(() => {
+    dispatch(fetchPostStylist());
+    dispatch(fetchPostService());
+  }, [dispatch]);
+
+  // Hàm thêm select nếu số lượng dưới 3
+  const handleAddSelect = (event) => {
+    event.preventDefault();
+    if (selects.length < 3) {
+      setSelects([...selects, selects.length + 1]);
+    }
+  };
+
+  // Hàm xóa select nếu số lượng trên 1
+  const handleRemoveSelect = (index) => {
+    if (selects.length > 1) {
+      // Remove the select
+      const newSelects = selects.filter((_, i) => i !== index);
+      setSelects(newSelects);
+
+      const newSelectedServices = [...selectedServices];
+      newSelectedServices.splice(index, 1);
+      setSelectedServices(newSelectedServices);
+    }
+  };
+
   return (
     <>
       <div className="container-fluid bg-dark">
@@ -77,13 +138,16 @@ function BookingPage() {
                     id="stylistSelect"
                     className="form-select"
                     aria-label="Select a stylist"
+                    onChange={handleStylistChange}
                   >
-                    <option selected="">Choose stylist</option>
-                    <option value={1}>Nguyễn Văn A</option>
-                    <option value={2}>Trần Văn B</option>
-                    <option value={3}>Võ Thị C</option>
-                    <option value={4}>Đào Tấn D</option>
-                    <option value={5}>Vũ Như E</option>
+                    <option selected>Choose stylist</option>
+                    {dataStylist.map((item) => {
+                      return (
+                        <option key={item.id} value={item.id}>
+                          {item.fullName}
+                        </option>
+                      );
+                    })}
                   </select>
                 </div>
                 {/* Stylist Profile */}
@@ -105,84 +169,7 @@ function BookingPage() {
                           </p>
                         </div>
                       </div>
-                      {/* Gallery */}
-                      <div className="mt-3">
-                        <div className="row g-2">
-                          {/* g-2 adds spacing between grid items */}
-                          <div className="col-6 col-sm-4 mb-2">
-                            <img
-                              src="public/assets/image/avatar.jpg"
-                              alt="Haircut Example"
-                              className="img-fluid rounded"
-                              style={{
-                                objectFit: "cover",
-                                width: 100,
-                                height: 100,
-                              }}
-                            />
-                          </div>
-                          <div className="col-6 col-sm-4 mb-2">
-                            <img
-                              src="public/assets/image/avatar.jpg"
-                              alt="Haircut Example"
-                              className="img-fluid rounded"
-                              style={{
-                                objectFit: "cover",
-                                width: 100,
-                                height: 100,
-                              }}
-                            />
-                          </div>
-                          <div className="col-6 col-sm-4 mb-2">
-                            <img
-                              src="public/assets/image/avatar.jpg"
-                              alt="Haircut Example"
-                              className="img-fluid rounded"
-                              style={{
-                                objectFit: "cover",
-                                width: 100,
-                                height: 100,
-                              }}
-                            />
-                          </div>
-                          <div className="col-6 col-sm-4 mb-2">
-                            <img
-                              src="public/assets/image/avatar.jpg"
-                              alt="Haircut Example"
-                              className="img-fluid rounded"
-                              style={{
-                                objectFit: "cover",
-                                width: 100,
-                                height: 100,
-                              }}
-                            />
-                          </div>
-                          <div className="col-6 col-sm-4 mb-2">
-                            <img
-                              src="public/assets/image/avatar.jpg"
-                              alt="Haircut Example"
-                              className="img-fluid rounded"
-                              style={{
-                                objectFit: "cover",
-                                width: 100,
-                                height: 100,
-                              }}
-                            />
-                          </div>
-                          <div className="col-6 col-sm-4 mb-2">
-                            <img
-                              src="public/assets/image/avatar.jpg"
-                              alt="Haircut Example"
-                              className="img-fluid rounded"
-                              style={{
-                                objectFit: "cover",
-                                width: 100,
-                                height: 100,
-                              }}
-                            />
-                          </div>
-                        </div>
-                      </div>
+
                       {/* Gallery End */}
                     </div>
                   </div>
@@ -194,38 +181,41 @@ function BookingPage() {
             <label htmlFor="inputPhone" className="form-label text-white">
               Services
             </label>
-            <select className="form-select" aria-label="Default select example">
-              <option selected="">Choose service</option>
-              <option value={1}>Haircut and shampooing</option>
-              <option value={2}>Coloring</option>
-              <option value={3}>Combo</option>
-            </select>
-            {/* Service END */}
-            <label htmlFor="inputPhone" className="form-label text-white mt-3">
-              Selected services
-            </label>
-            <ul className="list-group">
-              <li className="list-group-item d-flex justify-content-between align-items-center">
-                <div>
-                  <span className="fw-bold">Shaving</span>
-                  <span className="text-muted">x 2</span>
+            <div>
+              {selects.map((_, index) => (
+                <div key={index} className="d-flex align-items-center mb-2">
+                  <select
+                    className="form-select"
+                    onChange={(e) => handleServiceChange(index, e.target.value)}
+                  >
+                    <option value="" disabled>
+                      Choose service
+                    </option>
+                    {dataService.map((item) => (
+                      <option key={item.id} value={item.id}>
+                        {item.serviceName}
+                      </option>
+                    ))}
+                  </select>
+                  {index > 0 && (
+                    <button
+                      className="btn btn-danger ms-2"
+                      onClick={() => handleRemoveSelect(index)}
+                    >
+                      Xóa
+                    </button>
+                  )}
                 </div>
-                <span className="badge bg-secondary">150.000 VND</span>
-              </li>
-              <li className="list-group-item d-flex justify-content-between align-items-center">
-                <div>
-                  <span className="fw-bold">Shaving</span>
-                  <span className="text-muted">x 1</span>
-                </div>
-                <span className="badge bg-secondary">150.000 VND</span>
-              </li>
-            </ul>
-            <button
-              className="btn btn-dark mt-3"
-              style={{ width: "100%", border: "1px solid" }}
-            >
-              Add Service
-            </button>
+              ))}
+
+              <button
+                className="btn btn-dark mt-3"
+                style={{ width: "100%", border: "1px solid" }}
+                onClick={handleAddSelect}
+              >
+                Add Service
+              </button>
+            </div>
             <hr />
             <div className="row">
               <div className="col-md-6 text-white">
@@ -299,7 +289,6 @@ function BookingPage() {
               rows={5}
               id="comment"
               placeholder="Note"
-              defaultValue={""}
             />
             {/* NOTE END */}
             <button
