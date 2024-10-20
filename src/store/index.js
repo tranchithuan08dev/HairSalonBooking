26e83898f915a7c19987a771733ae11921bfd650
ChiThuan <tranchithuanjr@gmail.com>
2024-10-20 23:38:12 +0700

    create BOOKING

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import dashbroadSlice from "./dashbroadSlice";
 import homeSlice from "./homeSlice";
 import authReducer from "./authSlice";
+import bookingSlice from "./bookingSlice";
 
 const store = configureStore({
   reducer: {
     DASHBOARD: dashbroadSlice,
     AUTH: authReducer,
     HOME: homeSlice,
+    BOOKING: bookingSlice,
   },
 });
 
