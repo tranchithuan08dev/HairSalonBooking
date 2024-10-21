4c6cdbb8917887f6707f416a221b5b4b2df9e3eb
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-21 22:00:32 +0700

    update bookingServices

--- src/services/staffServices/bookingDetailStaffService.js
+++ src/services/staffServices/bookingDetailStaffService.js
@@ -4,12 +4,19 @@ const bookingService = {
     getAll: (page, perpage) => {
         return API.call().get(`api/v1/booking/getAll?page=${page}&perpage=${perpage}`);
     },
+    getTotalCount: () => {
+        return API.call().get(`api/v1/booking/getAll`); 
+    },
     getDetail: (id) => {
         return API.call().get(`api/v1/booking/detail?bookingID=${id}`);
     },
     updateBooking: (id, data) => {
         return API.call().patch(`api/v1/booking/update?bookingID=${id}`, data);
+    },
+    generateQR: (data) => {
+        return API.call().get(`api/v1/payment/generateQR`, data);
     }
+    
 }
 
 export default bookingService;
