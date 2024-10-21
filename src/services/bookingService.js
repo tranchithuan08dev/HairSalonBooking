9b19d5adf0a817daac5081face2f4ac1204c1b1b
ChiThuan <tranchithuanjr@gmail.com>
2024-10-22 02:03:38 +0700

    create api booking

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -4,5 +4,8 @@ const bookingService = {
   getWorkshfit: (stylistId) => {
     return API.call().get(`/api/v1/workshift/getWorkshfit?id=${stylistId}`);
   },
+  booking: (data) => {
+    return API.call().post(`/api/v1/booking/create`, data);
+  },
 };
 export default bookingService;
