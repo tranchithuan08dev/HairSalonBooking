e4f00edb8dea99e02dc7832169a86ac450aed8d9
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-23 11:56:41 +0700

    update services

--- src/services/StylistServices/BookingDetailService.js
+++ src/services/StylistServices/BookingDetailService.js
@@ -1,12 +1,18 @@
 import API from "../api";
 
 const bookingDetailService = {
-    getDetail: (id) => {
-        return API.call().get(`api/v1/booking/detail?bookingID=${id}`);
-    },
-    updateStatus: (id, data) => {
-        return API.call().get(`api/v1/booking/update?bookingID=${id}`,data);
-    }
-}
+  getDetail: (id) => {
+    return API.call().get(`api/v1/booking/detail?bookingID=${id}`);
+  },
+  getServiceDetail: (id) => {
+    return API.call().get(`api/v1/service/detail?id=${id}`);
+  },
+  getStylistDetail: (id) => {
+    return API.call().get(`api/v1/stylist/detail?id=${id}`);
+  },
+  updateStatus: (id, data) => {
+    return API.call().get(`api/v1/booking/update?bookingID=${id}`, data);
+  },
+};
 
 export default bookingDetailService;
--- src/services/StylistServices/WorkshiftService.js
+++ src/services/StylistServices/WorkshiftService.js
@@ -2,7 +2,7 @@ import API from "../api";
 
 const workshiftService = {
     getAll: (id) => {
-        return API.call().get(`api/v1/workshift/getWorkshfit?id=${id}`);
+        return API.call().get(`api/v1/workshift/getWorkshfitDetail?id=${id}`);
     }
 }
 
