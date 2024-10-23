19515cd06b63c367b0b8bdf87f529700aa9d7104
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-23 15:47:19 +0700

    update salarySlice

--- src/store/staffSlice/index.js
+++ src/store/staffSlice/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import profileStaffReducer from "./profileStaffSlice";
 import bookingReducer from "./bookingSlice";
+import salaryReducer from "./salarySlice";
 
 const staffReducer = combineReducers({
   profile: profileStaffReducer,
-  booking: bookingReducer
+  booking: bookingReducer,
+  salary: salaryReducer
 });
 export default staffReducer;
---
+++ src/store/staffSlice/salarySlice.js
@@ -0,0 +1,63 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import salaryService from "../../services/staffServices/salaryStaffService";
+
+const initialState = {
+    data: {},
+    loading: true,
+    error: null
+};
+
+const name = "salary";
+
+export const fetchData = createAsyncThunk(
+  `${name}/fetchData`, 
+  async (id) => {
+      try {
+          const responseSalary = await salaryService.getTotalSalary(id);
+          return {
+              ok: true,
+              salary: responseSalary.data.data
+          };
+      } catch (error) {
+          return {
+              ok: false,
+              message: "Fetch data error!",
+          };
+      }
+  }
+);
+
+const salarySlice = createSlice({
+  name,
+  initialState,
+  reducers: {
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload; 
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchData.pending, (state) => {
+        state.loading = true;
+    })
+    .addCase(fetchData.fulfilled, (state, action) => {
+        state.loading = false;
+        if(action.payload.ok){
+            state.data = {
+                salary: action.payload.salary,
+              };
+        }else{
+            state.error = action.payload.message;
+        }
+    })
+    .addCase(fetchData.rejected, (state, action) => {
+        state.error = action.payload.message;
+    })
+  },
+});
+
+export const {setLoading, setError} = salarySlice.actions;
+export default salarySlice.reducer; 
