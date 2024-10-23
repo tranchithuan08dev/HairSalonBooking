bd6ae0fcd2c4e43302989d5c88466144d8528a25
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-23 16:54:28 +0700

    update salarySlice

---
+++ src/store/stylistSlice/SalarySlice.js
@@ -0,0 +1,70 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import salaryService from "../../services/StylistServices/SalaryService";
+
+const initialState = {
+    data: {},
+    loading: true,
+    error: null
+};
+
+const name = "salary";
+
+export const fetchData = createAsyncThunk(
+  `${name}/fetchData`, 
+  async ({id, date}) => {
+      try {
+          const responseBooking = await salaryService.countBookings();
+          const matchingBookings = responseBooking.data.bookings.filter(
+            (booking) => booking.stylistID === id
+          )
+          const responseSalary = await salaryService.getTotalSalaryUntilNow(id, date);
+
+          return {
+              ok: true,
+              count: matchingBookings.length,
+              salary: responseSalary.data.data
+          };
+      } catch (error) {
+          return {
+              ok: false,
+              message: "Fetch data error!",
+          };
+      }
+  }
+);
+
+const salarySlice = createSlice({
+  name,
+  initialState,
+  reducers: {
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload; 
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchData.pending, (state) => {
+        state.loading = true;
+    })
+    .addCase(fetchData.fulfilled, (state, action) => {
+        state.loading = false;
+        if(action.payload.ok){
+            state.data = {
+                count: action.payload.count,
+                salary: action.payload.salary,
+              };
+        }else{
+            state.error = action.payload.message;
+        }
+    })
+    .addCase(fetchData.rejected, (state, action) => {
+        state.error = action.payload.message;
+    })
+  },
+});
+
+export const {setLoading, setError} = salarySlice.actions;
+export default salarySlice.reducer; 
