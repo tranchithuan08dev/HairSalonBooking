6f9f9364b01923c5191c4399a004bf2b0b3330f1
ChiThuan <tranchithuanjr@gmail.com>
2024-10-25 02:35:35 +0700

    dispatch fetch home news

--- src/components/Home/Newsblog/index.jsx
+++ src/components/Home/Newsblog/index.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Item from "../CardItem";
+import { fetchPostNews } from "../../../store/dashbroadSlice";
+import { fetchHomeNews } from "../../../store/homeSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function NewBlog() {
+  const [itemsPerPage, setItemsPerPage] = useState(3);
+  const dispatch = useDispatch();
+  const total = useSelector((state) => state.DASHBOARD.postNews);
+  const dataNews = useSelector((state) => state.HOME.news);
+  console.log("dataNes", dataNews);
+
+  useEffect(() => {
+    dispatch(fetchPostNews());
+    dispatch(fetchHomeNews({ perpage: itemsPerPage }));
+  }, [dispatch, itemsPerPage]);
+  const loadMore = () => {
+    setItemsPerPage((prev) => prev + 3);
+  };
   return (
     <>
       <div className="service" style={{ backgroundColor: "#2e2d2d" }}>
@@ -11,19 +27,18 @@ function NewBlog() {
         <div className="container mx-auto">
           {/* Row 1 */}
           <div className="row d-flex justify-content-center flex-wrap">
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
-            <Item />
+            {dataNews.map((item) => (
+              <Item key={item.id} data={item} slug="stylist" />
+            ))}
           </div>
         </div>
-        <div className="d-flex justify-content-center p-5">
-          <a href="#" className="loading">
-            Xem thêm
-          </a>
-        </div>
+        {total.length > itemsPerPage && (
+          <div className="d-flex justify-content-center p-5">
+            <button onClick={loadMore} className="loading">
+              Xem thêm
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
