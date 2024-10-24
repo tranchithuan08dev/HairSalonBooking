1368be4fc01aecde352f127b10c5b6e0ca928c0d
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-25 01:58:54 +0700

    update updateWorkshift

--- src/store/staffSlice/index.js
+++ src/store/staffSlice/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 import profileStaffReducer from "./profileStaffSlice";
 import bookingReducer from "./bookingSlice";
 import salaryReducer from "./salarySlice";
+import updateWorkshift from "./updateWorkshift";
 
 const staffReducer = combineReducers({
   profile: profileStaffReducer,
   booking: bookingReducer,
-  salary: salaryReducer
+  salary: salaryReducer,
+  updateWorkshift: updateWorkshift
 });
 export default staffReducer;
---
+++ src/store/staffSlice/updateWorkshift.js
@@ -0,0 +1,99 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import updateWorkshiftService from "../../services/staffServices/updateStylistWorkshiftService";
+
+const initialState = {
+  data: {},
+  error: null,
+  message: "",
+  showAlert: false,
+};
+
+const name = "updateWorkshift";
+
+export const getAll = createAsyncThunk(`${name}/getAll`, async (id) => {
+  try {
+    const response = await updateWorkshiftService.getAll(id);
+    console.log(response.data.data);
+    return {
+      ok: true,
+      data: response.data.data,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      message: "Cannot get all workshift for this stylist!",
+    };
+  }
+});
+
+export const updateStatus = createAsyncThunk(
+  `${name}/updateStatus`,
+  async (data) => {
+    try {
+      await updateWorkshiftService.updateStatus(data);
+      return {
+        ok: true,
+        success: "Updated successfully!",
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        message: "Cannot update!",
+      };
+    }
+  }
+);
+
+const updateWorkshiftSlice = createSlice({
+  name,
+  initialState,
+  reducers: {
+    setData: (state, action) => {
+      state.data = { ...state.data, ...action.payload };
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+    setMessage: (state, action) => {
+      state.message = action.payload;
+    },
+    setShowAlert: (state) => {
+      state.showAlert = !state.showAlert;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAll.pending, (state) => {
+        state.error = null;
+      })
+      .addCase(getAll.fulfilled, (state, action) => {
+        if (action.payload.ok) {
+          state.data = action.payload.data;
+        } else {
+          state.error = action.payload.message;
+        }
+      })
+      .addCase(getAll.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(updateStatus.pending, (state) => {
+        state.error = null;
+      })
+      .addCase(updateStatus.fulfilled, (state, action) => {
+        state.showAlert = true;
+        if (action.payload.ok) {
+          state.message = action.payload.success;
+        } else {
+          state.error = action.payload.message;
+        }
+      })
+      .addCase(updateStatus.rejected, (state, action) => {
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { setData, setError, setMessage, setShowAlert } =
+  updateWorkshiftSlice.actions;
+
+export default updateWorkshiftSlice.reducer;
