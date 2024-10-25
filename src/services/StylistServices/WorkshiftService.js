b89480abbf82e688179fb7b06d356184e628a2d5
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-25 12:17:09 +0700

    update services

--- src/services/StylistServices/WorkshiftService.js
+++ src/services/StylistServices/WorkshiftService.js
@@ -1,9 +1,16 @@
 import API from "../api";
 
 const workshiftService = {
-    getAll: (id) => {
-        return API.call().get(`api/v1/workshift/getWorkshfitDetail?id=${id}`);
+    getAllByStylistID: (id) => {
+        return API.call().get(`api/v1/workshift/getWorkshift?id=${id}`);
+    },
+    createWorkshift: (data) => {
+        return API.call().post(`api/v1/workshift/addStylist`, data);
+    },
+    getAllWorkshift: () => {
+        return API.call().get(`api/v1/workshift/getAll`);
     }
+
 }
 
 export default workshiftService;
