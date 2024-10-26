733e136d74768c19bf41da31934341dd56e8fd45
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-26 17:36:27 +0700

    update helpers

--- src/helpers/getBadgeClass.js
+++ src/helpers/getBadgeClass.js
@@ -1,12 +1,14 @@
 export const getBadgeClass = (status) => {
-    switch (status) {
-      case "In-progress":
-        return "bg-info";
-      case "Completed":
-        return "bg-success"; 
-      case "Cancelled":
-        return "bg-secondary";
-      default:
-        return "bg-light";
-    }
+  switch (status) {
+    case "In-progress":
+      return "bg-primary";
+    case "Completed":
+      return "bg-success";
+    case "Cancelled":
+      return "bg-secondary";
+      case "Done":
+        return "bg-warning";
+    default:
+      return "bg-light";
+  }
 };
