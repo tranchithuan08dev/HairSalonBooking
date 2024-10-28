c59607e3e5f8e48ebdc5d3e5b45acccf51cfb7bc
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-28 09:13:44 +0700

    update components and css

---
+++ src/components/Stylist/PrivateRoutes/index.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
+
+function PrivateRoutes() {
+    const { token, currentUser } = useSelector((state) => state.AUTH);
+    const isLogin = !!token
+    const isStylist = currentUser?.record.role === "Stylist" ; 
+    return (
+        <>
+            {isLogin && isStylist ? <Outlet/> : <Navigate to="/login" />}
+        </>
+    );
+}
+
+export default PrivateRoutes;
