54a575cdb46e1dbc8b28a4ebb0663b7f017df49a
ChiThuan <tranchithuanjr@gmail.com>
2024-10-29 13:22:34 +0700

    add token

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -1,5 +1,5 @@
 import API from "./api";
-
+const token = localStorage.getItem("ACCESS_TOKKEN");
 const bookingService = {
   createGuest: (data) => {
     return API.call().post(`/api/v1/guest/create`, data);
@@ -24,6 +24,13 @@ const bookingService = {
   },
   cancleBooking: (id) => {
     return API.call().delete(`/api/v1/booking/delete?bookingID=${id}`);
+  },
+  historyBooking: (id) => {
+    return API.call().get(`/api/v1/booking/history?id=${id}`, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
   },
 };
 export default bookingService;
