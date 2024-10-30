df6054a3d9a27e8b9484e3030773050be15ca0a4
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-31 01:24:18 +0700

    update services

--- src/services/staffServices/bookingDetailStaffService.js
+++ src/services/staffServices/bookingDetailStaffService.js
@@ -2,75 +2,93 @@ import API from "../api";
 const token = localStorage.getItem("ACCESS_TOKKEN");
 
 const bookingService = {
-    getAll: () => {
-        return API.call().get(`api/v1/booking/getAll`);
-    },
-    getDetail: (id) => {
-        return API.call().get(`api/v1/booking/detail?bookingID=${id}`,{
-            headers: {
-                token: `Bearer ${token}`,
-            }
-        });
-    },
-    updateBooking: (data) => {
-        console.log(data);
-        return API.call().patch(`api/v1/booking/update`, data, {
-            headers: {
-                token: `Bearer ${token}`,
-            }
-        });
-    },
-    generateQR: (data) => {
-        return API.call().post(`api/v1/payment/generateQR`, data);
-    },
-    getServiceDetail: (id) => {
-        return API.call().get(`api/v1/service/detail?id=${id}`, {
-            headers: {
-                token: `Bearer ${token}`,
-            }
-        });
-    },
-    getStylistDetail: (id) => {
-        return API.call().get(`api/v1/stylist/detail?id=${id}`,{
-            headers: {
-                token: `Bearer ${token}`,
-            }
-        });
-    },
-    updatePayment: (id, data) => {
-        return API.call().patch(`api/v1/payment/update?id=${id}`, data ,{
-            headers: {
-                token: `Bearer ${token}`,
-            }
-        });
-    },
-    getAllPayment: () => {
-        return API.call().get(`api/v1/payment/getAll`);
-    },
-    updateStatus: (data) => {
-        return API.call().patch(`api/v1/booking/change-status`, data, {
-            headers: {
-                token: `Bearer ${token}`,
-            }
-        });
-    },
-    getCustomer: (id) => {
-        return API.call().get(`api/v1/customer/detail?id=${id}`, {
-            headers: {
-                token: `Bearer ${token}`,
-            }
-        })
-    },
-    getAllServices: () => {
-        return API.call().get(`api/v1/service/getAll`);
-    },
-    updateCustomer: (id, data) => {
-        return API.call().patch(`api/v1/customer/update?id=${id}`, data, {
-            headers: {
-                token: `Bearer ${token}`,
-                "Content-Type": "multipart/form-data"
-            }
-        });
+  getAll: () => {
+    return API.call().get(`api/v1/booking/getAll`);
+  },
+  getDetail: (id) => {
+    return API.call().get(`api/v1/booking/detail?bookingID=${id}`, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
+  updateBooking: (data) => {
+    console.log(data);
+    return API.call().patch(`api/v1/booking/update`, data, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
+  generateQR: (data) => {
+    return API.call().post(`api/v1/payment/generateQR`, data);
+  },
+  getServiceDetail: (id) => {
+    return API.call().get(`api/v1/service/detail?id=${id}`, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
+  getStylistDetail: (id) => {
+    return API.call().get(`api/v1/stylist/detail?id=${id}`, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
+  updatePayment: (id, data) => {
+    return API.call().patch(`api/v1/payment/update?id=${id}`, data, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
+  getAllPayment: () => {
+    return API.call().get(`api/v1/payment/getAll`);
+  },
+  updateStatus: (data) => {
+    return API.call().patch(`api/v1/booking/change-status`, data, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
+  getCustomer: (id) => {
+    return API.call().get(`api/v1/customer/detail?id=${id}`, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
+  getAllServices: () => {
+    return API.call().get(`api/v1/service/getAll`);
+  },
+  updateCustomer: (id, data) => {
+    return API.call().patch(`api/v1/customer/update?id=${id}`, data, {
+      headers: {
+        token: `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  },
+  createPaymentUrl: (data) => {
+    const params = new URLSearchParams();
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        params.append(key, data[key]);
+      }
     }
-}
+    return API.call().post(
+      "api/v1/payment/create_payment_url",
+      params.toString(),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+           token: `Bearer ${token}`,
+        },
+      }
+    );
+  },
+};
 export default bookingService;
