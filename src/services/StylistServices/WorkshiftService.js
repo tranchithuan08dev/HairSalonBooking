041431aa102be34f4e5bcf7b049d3aad59a7e798
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-30 09:40:45 +0700

    update services stylist

--- src/services/StylistServices/WorkshiftService.js
+++ src/services/StylistServices/WorkshiftService.js
@@ -1,27 +1,34 @@
 import API from "../api";
 const token = localStorage.getItem("ACCESS_TOKKEN");
 
-
 const workshiftService = {
-    getAllByStylistID: (id) => {
-        return API.call().get(`api/v1/workshift/getWorkshift?id=${id}`);
-    },
-    createWorkshift: (data) => {
-        return API.call().post(`api/v1/workshift/addStylist`, data, {
-            headers: {
-                token: `Bearer ${token}`,
-            }
-        });
-    },
-    getAllWorkshift: () => {
-        return API.call().get(`api/v1/workshift/getAll`);
-    },
-    getAllDetailByID: (id) => {
-        return API.call().get(`api/v1/workshift/getWorkshiftDetail?id=${id}`);
-    },
-    getAllBooking: (id) => {
-        return API.call().get(`api/v1/booking/getAll`);
-    }
-}
+  getAllByStylistID: (id) => {
+    return API.call().get(`api/v1/workshift/getWorkshift?id=${id}`, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
+  createWorkshift: (data) => {
+    return API.call().post(`api/v1/workshift/addStylist`, data, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
+  getAllWorkshift: () => {
+    return API.call().get(`api/v1/workshift/getAll`);
+  },
+  getAllDetailByID: (id) => {
+    return API.call().get(`api/v1/workshift/getWorkshiftDetail?id=${id}`, {
+      headers: {
+        token: `Bearer ${token}`,
+      },
+    });
+  },
+  getAllBooking: (id) => {
+    return API.call().get(`api/v1/booking/getAll`);
+  },
+};
 
 export default workshiftService;
