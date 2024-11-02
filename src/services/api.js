7484ab772e25290c4d269c3da53859609de1e0e6
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-11-02 14:27:13 +0700

    update services

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,25 @@ import axios from "axios";
 
 const API = {
   call: function () {
-    return axios.create({
-      baseURL: "http://localhost:5000/",
+    const instance = axios.create({
+        baseURL: "http://localhost:5000/",
     });
-  },
+
+    instance.interceptors.request.use(
+        (config) => {
+            const token = localStorage.getItem("ACCESS_TOKKEN");
+            if (token) {
+                config.headers["token"] = `Bearer ${token}`;
+            }
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    );
+
+    return instance;
+},
   callWithToken: function (token) {
     if (!token) token = localStorage.getItem("ACCESS_TOKKEN");
     return axios.create({
